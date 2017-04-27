876786eb107d59fcd8be4533b637f408b91182bd
FABIANZEA <fabian.zea@uptc.edu.co>
2017-04-27 14:45:57 -0500

    LABORATORIO #2 MICROC

---
+++ tempo_final.X/build/default/production/tempo_final.o.d
@@ -0,0 +1,24 @@
+build/default/production/tempo_final.o:  \
+	D:/MICROC/LAB2/temporizador_completo/tempo_final.X/tempo_final.asm  \
+	D:/MICROC/LAB2/temporizador_completo/tempo_final.X/TABLA.inc  \
+	D:/MICROC/LAB2/temporizador_completo/tempo_final.X/MENSAJE_1.inc  \
+	D:/MICROC/LAB2/temporizador_completo/tempo_final.X/MENSAJE_2.inc  \
+	D:/MICROC/LAB2/temporizador_completo/tempo_final.X/MENSAJE_3.inc  \
+	D:/MICROC/LAB2/temporizador_completo/tempo_final.X/MENSAJE_4.inc  \
+	D:/MICROC/LAB2/temporizador_completo/tempo_final.X/MENSAJE_5.inc  \
+	D:/MICROC/LAB2/temporizador_completo/tempo_final.X/TABLAESPERA.inc  \
+	D:/MICROC/LAB2/temporizador_completo/tempo_final.X/CONFIGURACION.inc  \
+	D:/MICROC/LAB2/temporizador_completo/tempo_final.X/CONTADORDEC.inc  \
+	D:/MICROC/LAB2/temporizador_completo/tempo_final.X/CONTADORINC.inc  \
+	D:/MICROC/LAB2/temporizador_completo/tempo_final.X/CONVERSOR1.inc  \
+	D:/MICROC/LAB2/temporizador_completo/tempo_final.X/CONVERSOR2.inc  \
+	D:/MICROC/LAB2/temporizador_completo/tempo_final.X/CONVERSOR3.inc  \
+	D:/MICROC/LAB2/temporizador_completo/tempo_final.X/INIG_REG.inc  \
+	D:/MICROC/LAB2/temporizador_completo/tempo_final.X/RETARDO1.inc  \
+	D:/MICROC/LAB2/temporizador_completo/tempo_final.X/T_ESCOGER.inc  \
+	D:/MICROC/LAB2/temporizador_completo/tempo_final.X/ROTAR.inc  \
+	D:/MICROC/LAB2/temporizador_completo/tempo_final.X/SALIDA_VISUAL.inc  \
+	D:/MICROC/LAB2/temporizador_completo/tempo_final.X/ROTAR_CARACTER.inc  \
+	D:/MICROC/LAB2/temporizador_completo/tempo_final.X/PUBLIC.inc  \
+	D:/MICROC/LAB2/temporizador_completo/tempo_final.X/ESPERA.inc  \
+
---
+++ tempo_final.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=tempo_final.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/tempo_final.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=tempofinal.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/tempofinal.x.tar
